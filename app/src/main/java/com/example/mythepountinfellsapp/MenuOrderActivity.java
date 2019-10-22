a1625e5a5098f88a617861c1ee69a5f75b3d6a05
herberthG <1210441@bcpss.org>
2019-10-22 16:54:42 -0400

    add sub total and total

--- app/src/main/java/com/example/mythepountinfellsapp/MenuOrderActivity.java
+++ app/src/main/java/com/example/mythepountinfellsapp/MenuOrderActivity.java
@@ -3,15 +3,19 @@ package com.example.mythepountinfellsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MenuOrderActivity extends AppCompatActivity {
 
-    EditText OrderQuantity;
-    TextView OrderDollar;
-
+    EditText orderQuantityET;
+    TextView orderDollarTV;
+    Button addDollarBT;
+    Button addTaxBT;
+    TextView totalDollarTV;
+    int subtotal;
 
 
 
@@ -21,8 +25,32 @@ public class MenuOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu_order);
 
 
-        OrderQuantity = (EditText) findViewById(R.id.OrderQuantity);
-        OrderDollar = (TextView) findViewById(R.id.OrderDollar);
+        orderQuantityET = (EditText) findViewById(R.id.OrderQuantity);
+        orderDollarTV = (TextView) findViewById(R.id.OrderDollar);
+        addDollarBT = (Button) findViewById(R.id.AddDollar);
+        addTaxBT = (Button) findViewById(R.id.AddTax);
+        totalDollarTV = (TextView) findViewById(R.id.TotalDollar);
+
+        addDollarBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int orderQuantity=Integer.parseInt(orderQuantityET.getText().toString());
+                subtotal=orderQuantity*10;
+                orderDollarTV.setText("$"+subtotal);
+            }
+        });
+
+        addTaxBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double tax=subtotal*0.06;
+                double total=subtotal+tax;
+                totalDollarTV.setText("$"+total);
+
+            }
+        });
+
+
     }
 
 }
